6955a29bb1295a2475c2b1125fc805e1123c4641
Ngydm <nagy.adam_ny22@petrik.hu>
2026-01-30 09:48:37 +0100

    Evidencekezd

---
+++ Digit nyomzo/Evidence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Evidence
+	{
+		private int id;
+		private string type;
+		private string description;
+		private int trust;
+
+		public Evidence(int id, string type, string description, int trust)
+		{
+			this.id = id;
+			this.type = type;
+			this.description = description;
+			this.trust = trust;
+		}
+
+		public int Id { get => id; set => id = value; }
+		public string Type { get => type; set => type = value; }
+		public string Description { get => description; set => description = value; }
+		public int Trust { get => trust; set => trust = value; }
+	}
+}
