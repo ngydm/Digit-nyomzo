7f8ae6c5133a75854003841bce7e53b8f76348f0
Ngydm <nagy.adam_ny22@petrik.hu>
2026-01-30 09:29:17 +0100

    Witness kezdet

---
+++ Digit nyomzo/Witness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Witness
+	{
+		private Person WPerson;
+		private string confession;
+		private DateOnly TimeoConfession;
+
+		public Witness(Person wPerson, string confession, DateOnly timeoConfession)
+		{
+			WPerson = wPerson;
+			this.confession = confession;
+			TimeoConfession = timeoConfession;
+		}
+
+		public string Confession { get => confession; set => confession = value; }
+		public DateOnly TimeoConfession1 { get => TimeoConfession; set => TimeoConfession = value; }
+		internal Person WPerson1 { get => WPerson; set => WPerson = value; }
+	}
+}
