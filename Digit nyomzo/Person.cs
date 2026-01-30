619747bfcb48866dbeaf0c25c9a40b4196e88ae4
Ngydm <nagy.adam_ny22@petrik.hu>
2026-01-30 09:26:47 +0100

    suspect kezdet

---
+++ Digit nyomzo/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Person
+	{
+		private string name;
+		private int age;
+		private string note;
+
+		public Person(string name, int age, string note)
+		{
+			this.name = name;
+			this.age = age;
+			this.note = note;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Age { get => age; set => age = value; }
+		public string Note { get => note; set => note = value; }
+	}
+}
---
+++ Digit nyomzo/Suspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Suspect
+	{
+		private Person person;
+		private int suslvl;
+		private string statusz;
+
+		public Suspect(Person person, int suslvl, string statusz)
+		{
+			this.person = person;
+			this.suslvl = suslvl;
+			this.statusz = statusz;
+		}
+
+		public int Suslvl { get => suslvl; set => suslvl = value; }
+		public string Statusz { get => statusz; set => statusz = value; }
+		internal Person Person { get => person; set => person = value; }
+	}
+}
