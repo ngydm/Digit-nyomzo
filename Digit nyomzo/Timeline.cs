c351a26c396b4951d909190029961b12356d6100
Ngydm <nagy.adam_ny22@petrik.hu>
2026-01-30 09:50:09 +0100

    timeline kezd

---
+++ Digit nyomzo/Timeline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Timeline
+	{
+		private DateOnly date;
+		private string description;
+	}
+}
