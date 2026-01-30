f8ba72afda5612d0e561edd777f5ea21f2e88040
Ngydm <nagy.adam_ny22@petrik.hu>
2026-01-30 09:00:03 +0100

    https://github.com/ngydm/Digit-nyomzo.git kész

---
+++ Digit nyomzo/Felhasznalo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_nyomzo
+{
+	internal class Felhasznalo
+	{
+		private string nev;
+		private string azonosito;
+		private string pozicio;
+
+		public Felhasznalo(string nev, string azonosito, string pozicio)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.pozicio = pozicio;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		public string Pozicio { get => pozicio; set => pozicio = value; }
+	}
+}
